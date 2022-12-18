8bf787b16604587b93554b1e9a62e35e4f8f5af6
choi <doosan2058@gmail.com>
2022-12-18 23:01:59 +0900

    최원석 노트북 - 스케줄러 크론 오타 수정

--- src/main/java/com/goott/scheduler/Scheduler.java
+++ src/main/java/com/goott/scheduler/Scheduler.java
@@ -12,7 +12,7 @@ import javax.inject.Inject;
 public class Scheduler {
     @Inject
     SchedulerService schedulerService;
-    @Scheduled(cron = "0 00 00 * * *") // 매일 새벽 00:00:00 작업
+    @Scheduled(cron = "0 0 0 * * *") // 매일 새벽 00:00:00 작업
     public void autoUpdate(){
         // 회원 상품 구매시 예정 도착 시간이 지나면 상품 배송상태를 배송중 -> 배송완료 로 업데이트
         schedulerService.autoUpdateDeliveryState();
